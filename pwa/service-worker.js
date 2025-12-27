f36a0c4d48048d289da5ce6b0c5fa6733da82d51
rdrgosnto <rdrgo.snto@hotmail.com>
2025-12-27 08:32:38 -0300

    Create service-worker.js

---
+++ pwa/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'planner-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalando o service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativando o service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      })
+    ))
+  );
+});
+
+// Interceptando requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
