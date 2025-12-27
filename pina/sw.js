6c81dbf228b3e72bed21526709319f2845c8506b
rdrgosnto <rdrgo.snto@hotmail.com>
2025-12-26 22:44:03 -0300

    Update sw.js

--- pina/sw.js
+++ pina/sw.js
@@ -1,41 +1,9 @@
-const CACHE_NAME = "pina-pwa-v1";
-
-const ASSETS = [
-  "./",
-  "./pina.html",
-  "./pina_.png",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(k => k !== CACHE_NAME)
-          .map(k => caches.delete(k))
-      )
-    )
-  );
+self.addEventListener("activate", e => {
+  clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  const { request } = event;
-
-  // Nunca cachear chamadas da API
-  if (request.url.includes("powerplatform.com")) {
-    return;
-  }
-
-  event.respondWith(
-    caches.match(request).then(response =>
-      response || fetch(request)
-    )
-  );
-});
+self.addEventListener("fetch", () => {});
